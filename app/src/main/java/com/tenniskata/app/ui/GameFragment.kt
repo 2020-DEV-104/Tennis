5fd8a3f8d2d97a9f785726bcb7fc731c8f469d56
jeprubio@gmail.com <Jep Rubio>
2020-07-28 15:45:40 +0200

    [Add] Game Fragment

---
+++ app/src/main/java/com/tenniskata/app/ui/GameFragment.kt
@@ -0,0 +1,28 @@
+package com.tenniskata.app.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.tenniskata.app.R
+import com.tenniskata.app.viewmodel.GameViewModel
+
+class GameFragment : Fragment() {
+    private lateinit var viewModel: GameViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.game_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/tenniskata/app/viewmodel/GameViewModel.kt
@@ -0,0 +1,7 @@
+package com.tenniskata.app.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
