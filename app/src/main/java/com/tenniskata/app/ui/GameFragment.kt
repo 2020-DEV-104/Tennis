bb45c2f7300360a9204c1fd4f85f9006736cd089
jeprubio@gmail.com <Jep Rubio>
2020-07-28 16:48:26 +0200

    [Add] View bindings to test the app

--- app/src/main/java/com/tenniskata/app/ui/GameFragment.kt
+++ app/src/main/java/com/tenniskata/app/ui/GameFragment.kt
@@ -1,28 +1,35 @@
 package com.tenniskata.app.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.tenniskata.app.R
+import com.tenniskata.app.databinding.GameFragmentBinding
 import com.tenniskata.app.viewmodel.GameViewModel
 
 class GameFragment : Fragment() {
+
     private lateinit var viewModel: GameViewModel
+    private lateinit var binding: GameFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.game_fragment, container, false)
-    }
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.game_fragment, container, false
+        )
+        binding.lifecycleOwner = this
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        binding.viewModel = viewModel
+
+        return binding.root
     }
 
 }
